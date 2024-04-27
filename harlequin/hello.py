97ac1ae86cc35092b04632ee36342e5430c08de2
lparolari <luca.parolari23@gmail.com>
2024-04-27 14:51:52 +0200

    chore: remove hello world

--- harlequin/hello.py
+++
@@ -1,2 +0,0 @@
-def greet(name: str):
-    return f"Hello, {name}!"
--- tests/test_hello.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from harlequin.hello import greet
-
-
-class TestHello(unittest.TestCase):
-    def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World!")
